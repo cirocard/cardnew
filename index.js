3139633c108b8bbe5f6b633fdacc4b62e3bc5d62
cirocard <cirocard12@hotmail.com>
2019-07-07 21:20:32 -0300

    chore: alterado msg

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require('express');
 
 const app = express();
+
+app.get('/teste', (req, res) => {
+  return res.json({
+    hello: " msge modificada"
+  })
+});
+
+app.listen(3333);
